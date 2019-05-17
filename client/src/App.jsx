ae61b9e145b0a497ab263e1cdac3bccdbe00c3a6
Yerin Cha <lenacha32@gmail.com>
2019-05-16 23:43:33 -0700

    Start to work on server and api calls on the client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,95 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
-class Booking_Module extends React.Component {
-  constructor(props){
+
+class App extends React.Component {
+  constructor(props) {
     super(props);
+    this.state = {
+      roomInfo: {
+        roomname: '',
+        price: 0,
+        cleaning_fee: 0,
+        service_fee: 0,
+        tax: 0,
+        max_guest: null,
+        min_night: 0,
+        max_night: 0,
+        ratings: 0,
+        num_reviews: 0,
+      },
+      bookingInfo: {},
+    };
+
+    this.getRoomData = this.getRoomData.bind(this);
+    this.getBookingData = this.getBookingData.bind(this);
+    this.upDateRoomState = this.upDateRoomState.bind(this);
   }
+
+  componentDidMount() {
+    this.getRoomData(1);
+    this.getBookingData(1);
+  }
+
+  getRoomData(roomId) {
+    $.ajax({
+      url: `/room?roomId=${roomId}`,
+      type: 'GET',
+      error: (err) => {
+        throw err;
+      },
+      success: (result) => {
+        this.upDateRoomState(result);
+      },
+    });
+  }
+
+  getBookingData(roomId) {
+    $.ajax({
+      url: `/booking?roomId=${roomId}`,
+      type: 'GET',
+      error: (err) => {
+        throw err;
+      },
+      success: (result) => {
+        console.log(result);
+      },
+    });
+  }
+
+  upDateRoomState(result) {
+    this.setState({
+      roomInfo: {
+        roomname: result.roomname,
+        price: result.price,
+        cleaning_fee: result.cleaning_fee,
+        service_fee: result.service_fee,
+        tax: result.tax,
+        max_guest: result.max_guest,
+        min_night: result.min_night,
+        max_night: result.max_night,
+        ratings: result.ratings,
+        num_reviews: result.num_reviews,
+      },
+    });
+    console.log(this.state);
+  }
+
+  // upDateBookingState(result) {
+  //   this.setState({
+  //     bookingInfo: {
+  //     },
+  //   });
+  //   console.log(this.state);
+  // }
 
   render() {
     return (
-      <div></div>
+      <div>
+        <div>Hello</div>
+      </div>
     );
   }
 }
-ReactDOM.render(<Booking_Module />, document.getElementById('booking'));
+ReactDOM.render(<App />, document.getElementById('booking'));
--- server/index.js
+++ server/index.js
@@ -1,21 +1,41 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const db = require('../db/dataGenerator.js');
 
 const app = express();
 const port = 3000;
+app.use(express.static(path.join(__dirname, '../public/dist')));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(__dirname + './public/dist'));
-
-app.get('/booking', (req, res) => {
-  console.log(req.body);
-  res.send('hi');
-});
 
 app.get('/room', (req, res) => {
-  console.log(req.body);
-  res.send('hi');
+  db.Room.findAll({
+    where: {
+      id: req.query.roomId,
+    },
+  })
+    .then((result) => {
+      res.send(result[0].dataValues);
+    })
+    .catch(() => {
+      res.sendStatus(500);
+    });
+});
+
+app.get('/booking', (req, res) => {
+  db.Booking.findAll({
+    where: {
+      roomId: 1,
+    },
+  })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch(() => {
+      res.sendStatus(500);
+    });
 });
 
 app.post('/', (req, res) => {
