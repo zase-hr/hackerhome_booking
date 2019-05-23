80782357daf0488300c3171d5988197cea9e7801
Yerin Cha <lenacha32@gmail.com>
2019-05-23 16:10:14 -0700

    Add svg tags for guest picker

--- client/src/components/Cost.jsx
+++ client/src/components/Cost.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class Cost extends React.Component {
   constructor(props) {
--- client/src/components/Guest.jsx
+++ client/src/components/Guest.jsx
@@ -6,10 +6,46 @@ import GuestPicker from './GuestPicker.jsx';
 const Guest = props => (
   <div className="guest">
     <div className="guestSection">Guests</div>
-    <button className={props.guestExpand ? 'guestButton' : 'guestButton0'} type="button" expand={props.guestSelected.toString()} onClick={props.guestExpandToggle}>
-      <div className="guestButton1"><div className="message">{props.message}</div></div>
-      <div className="goDown" />
-    </button>
+    <div className="guestSection1">
+      <button className={props.guestExpand ? 'guestButton' : 'guestButton0'} type="button" expand={props.guestSelected.toString()} onClick={props.guestExpandToggle}>
+        <div className="guestButton1">
+          <div className="message">
+            {props.message}
+            <div className="goDown">
+              {props.guestExpand
+                ? (
+                  <svg
+                    viewBox="0 0 18 18"
+                    role="presentation"
+                    aria-hidden="true"
+                    focusable="false"
+                    style={{
+                      height: '16px', width: '16px', display: 'block', fill: 'currentcolor',
+                    }}
+                  >
+                    <path d="m1.71 13.71a1 1 0 1 1 -1.42-1.42l8-8a1 1 0 0 1 1.41 0l8 8a1 1 0 1 1 -1.41 1.42l-7.29-7.29z" fillRule="evenodd" />
+
+                  </svg>
+                )
+                : (
+                  <svg
+                    viewBox="0 0 18 18"
+                    role="presentation"
+                    aria-hidden="true"
+                    focusable="false"
+                    style={{
+                      height: '16px', width: '16px', display: 'block', fill: 'currentcolor',
+                    }}
+                  >
+                    <path d="m16.29 4.3a1 1 0 1 1 1.41 1.42l-8 8a1 1 0 0 1 -1.41 0l-8-8a1 1 0 1 1 1.41-1.42l7.29 7.29z" fillRule="evenodd" />
+                  </svg>
+                )}
+            </div>
+          </div>
+        </div>
+
+      </button>
+    </div>
     <div className="guestPicker">
       {props.guestExpand
         ? (
@@ -18,14 +54,8 @@ const Guest = props => (
             num_children={props.num_children}
             infants={props.infants}
             guest={props.guest}
-            // increaseAdults={props.increaseAdults}
-            // increaseChildren={props.increaseChildren}
-            // increaseInfants={props.increaseInfants}
             increaseGuest={props.increaseGuest}
             decreaseGuest={props.decreaseGuest}
-            // decreaseAdults={props.decreaseAdults}
-            // decreaseChildren={props.decreaseChildren}
-            // decreaseInfants={props.decreaseInfants}
             guestButtonMessage={props.guestButtonMessage}
             guestExpandToggle={props.guestExpandToggle}
             updateTotalNights={props.updateTotalNights}
