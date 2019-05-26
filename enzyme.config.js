744f6bac2ad5a69eca26f8f2ce1df3755a31e311
Yerin Cha <lenacha32@gmail.com>
2019-05-25 23:45:27 -0700

    Implement tests

--- enzyme.config.js
+++ enzyme.config.js
@@ -1,4 +1,14 @@
-import { configure } from 'enzyme';
+import React from 'react';
+import {
+  shallow, render, mount, configure,
+} from 'enzyme';
+import sinon from 'sinon';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+global.React = React;
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+global.sinon = sinon;
